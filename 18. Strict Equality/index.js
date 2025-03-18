99f70e1863fc3f81d18445fba8f0c43f99b8283e
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 16:38:09 +0530

    Strict Equality

---
+++ 18. Strict Equality/index.js
@@ -0,0 +1,14 @@
+// = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14;
+
+// In JavaScript 3.14 and "3.14" are equal
+if(PI !== "3.14"){
+    console.log("That is not PI");
+}else{
+    console.log("That is PI");
+}

0833c15e377cc014fec2a0f09d76798b024ac7d1
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 08:36:38 +0530

    3. Arithematic Operators

--- 3. Arithematic Operators/index.js
+++ 3. Arithematic Operators/index.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+// arithematic operators = operands (values, variables, etc.)
+//              operators (+ - * /)
+//              ex. 11 = x + 5;
+
+/**
+ * Operator precedence
+ * 1. paranthesis ()
+ * 2. exponents
+ * 3. multiplication & division & modulo
+ * 4. addition and subtraction
+ */
+
+let students = 30;
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 8;
+
+// students += 2;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+
+
+console.log(students);
+
+
+let result = 1 + 2 * 3 + 4 ** 2;
+result = 12 % 5 + 8 / 2;
+console.log(result);

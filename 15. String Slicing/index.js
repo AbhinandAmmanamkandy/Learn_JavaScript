118c4234b3223a89e12eec23cf5025f78c2b554f
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 15:54:09 +0530

    String Slicing

---
+++ 15. String Slicing/index.js
@@ -0,0 +1,25 @@
+// String slicing - creating a substring from a portion of 
+//              another string
+
+//              string.slice(start, end)
+
+// const fullName = "Ghost Reborn";
+// // let firstName = fullName.slice(0, 5); // Ending index is exclusive
+// // let lastName = fullName.slice(6);
+
+// // let firstChar = fullName.slice(0, 1);
+// // let lastChar = fullName.slice(-1)
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1)
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const email = "ghost1@gmail.com";
+
+// let userName = email.slice(0, email.indexOf("@"));
+// let extension = email.slice(email.indexOf("@") + 1);
+
+// console.log(userName);
+// console.log(extension);

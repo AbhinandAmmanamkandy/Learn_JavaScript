f089faff048100e7c0449d4325f26683cabe8677
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 15:44:34 +0530

    String Methods

---
+++ 14. String Methods/index.js
@@ -0,0 +1,21 @@
+// String methods - allows you to manupulate and work with text (strings)
+
+let userName = " Ghost ";
+
+// userName.charAt(0);
+// userName.indexOf("o");
+// userName.lastIndexOf("o");
+// console.log(userName.length);
+// userName = userName.trim();
+// userName = userName.toUpperCase();
+// userName = userName.toLowerCase();
+// userName.repeat(3);
+// userName.startsWith(" ");
+// userName.endsWith(" ");
+// userName.includes(" ");
+
+let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.replaceAll("-", "");
+// phoneNumber = phoneNumber.padStart(15, "0");
+// phoneNumber = phoneNumber.padEnd(15, "0");
+console.log(phoneNumber);

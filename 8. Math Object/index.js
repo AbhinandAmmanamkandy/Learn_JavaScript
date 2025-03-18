a19b4b0f7546b56878e823d997ce7abc499ebc14
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 11:09:56 +0530

    Math Object

---
+++ 8. Math Object/index.js
@@ -0,0 +1,28 @@
+// Math - built in object that provides a 
+//      collection of properties and methods
+
+
+console.log(Math.PI)
+console.log(Math.E);
+
+let x = 3.21;
+let y = 2;
+let z = 2;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x); // Removes decimal
+// z = Math.pow(x, y);
+// z = Math.sqrt(81);
+// z = Math.log(10);
+// z = Math.sin(45);
+// z = Math.cos(45);
+// z = Math.tan(45);
+// z = Math.abs(x);
+// z = Math.sign(x);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
+// console.log(max);
+// console.log(min);
+// console.log(z);

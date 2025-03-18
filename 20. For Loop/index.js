97580bffddc8f44a102f32f250dde272643016bd
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-18 17:02:18 +0530

    For Loop

---
+++ 20. For Loop/index.js
@@ -0,0 +1,14 @@
+// for loop - repeat some code a LIMITED amount of times
+
+// for(let i=3; i>0; i--){
+//     console.log(i);
+// }
+
+// console.log("Happy New Year");
+
+for(let i=1; i<=20; i++){
+    if(i == 13){
+        break;
+    }
+    console.log(i);
+}

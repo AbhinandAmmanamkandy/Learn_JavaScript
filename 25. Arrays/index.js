63dbcf41efb9c27253779904084ad358b4532a32
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-19 12:44:51 +0530

    Arrays

---
+++ 25. Arrays/index.js
@@ -0,0 +1,25 @@
+// array - a variable like structure that can hold more than 1 value
+
+let fruits = ["apple", "orange", "banana"];
+
+// fruits[10] = "coconut"; // increases array size to 10 and everything else will be empty
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango"); // Adds to beginning of the array
+// fruits.shift(); // Remove element from beginning
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("orange");
+// let index = fruits.indexOf("jackfruit"); // Returns -1
+
+// for (let i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// for (let fruit of fruits){
+//     console.log(fruit);
+// }
+
+// Sort
+// fruits.sort();
+// fruits.sort().reverse();

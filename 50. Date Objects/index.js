6c13041f3b0e99239c9981daf8f856ed4f943b64
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-20 10:13:32 +0530

    Dates

---
+++ 50. Date Objects/index.js
@@ -0,0 +1,29 @@
+// Date(year, month, day, hour, minute, secons, ms)
+
+// const date = new Date(2024, 0, 1, 2, 3, 4, 5);
+// const date = new Date("2024-01-02T12:00:00Z")
+// const date = new Date(1700000000000);
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hour = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const dayOfWeek = date.getDay();
+
+// date.setFullYear(2024);
+// date.setMonth(0);
+// date.setDate(1);
+// date.setHours(2);
+// date.setMinutes(3);
+// date.setSeconds(4);
+// date.setMilliseconds(5);
+
+// const date1 = new Date("2023-12-31");
+// const date2 = new Date("2023-12-30");
+// if(date2>date1){
+//     console.log("Happy new year");
+// }
+
+// console.log(dayOfWeek);
